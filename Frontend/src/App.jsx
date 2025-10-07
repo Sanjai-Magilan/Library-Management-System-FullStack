a13c43e8c3614368455e616b546c13a661b86038
Sanjai-Magilan <sanjai6115@gmail.com>
2025-10-07 06:27:21 +0530

    simple frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,7 +5,7 @@ import SearchBar from "./components/SearchBar";
 export default function App() {
   return (
     <div className="searchPage">
-      <h1>Library</h1>
+      <h1 className= "arcade">Library</h1>
       <SearchBar />
     </div>
   );
