9a32d11f05723217101fc3432a0a244159e1164b
Sanjai-Magilan <sanjai6115@gmail.com>
2025-10-08 01:25:15 +0530

    login as guest done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
 import Login from "./components/login";
 export default function App() {
   return (
-    // <div className="searchPage">
-    //   <h1 className= "arcade">Library</h1>
-    //   <SearchBar />
-    // </div>
-    <div>
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/SearchBar" element={<SearchBar />} />
+      </Routes>
+    </Router>
   );
 }
--- Frontend/src/components/login.jsx
+++ Frontend/src/components/login.jsx
@@ -1,20 +1,38 @@
 import React from "react";
-//import { useState } from "react";
+import { useState } from "react";
 import "./login.css";
-//import user_icon from "../assets/person.png";
-//import email_icon from "../assets/email.png";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const goTo = () => {
+    navigate("/SearchBar");
+  };
   return (
     <div className="login-container">
       <div className="header">
         <h1 className="text-head">Welcome Back</h1>
         <h1 className="text">Email ID</h1>
-        <input type="text" className="mail" />
+        <input
+          type="email"
+          className="mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <h1 className="text">Password</h1>
-        <input type="password" className="mail" />
+        <input
+          type="password"
+          className="mail"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <button className="button-style">Login</button>
-      <button className="text-button">Continue as Guest</button>
+      <button className="text-button" onClick={goTo}>
+        Continue as Guest
+      </button>
     </div>
   );
 }
