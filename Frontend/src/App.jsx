49fcce943361a41404a2db70296e7effca727141
Sanjai-Magilan <sanjai6115@gmail.com>
2025-10-07 17:38:43 +0530

    simple login page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
-
+import Login from "./components/login";
 export default function App() {
   return (
-    <div className="searchPage">
-      <h1 className= "arcade">Library</h1>
-      <SearchBar />
+    // <div className="searchPage">
+    //   <h1 className= "arcade">Library</h1>
+    //   <SearchBar />
+    // </div>
+    <div>
+      <Login/>
     </div>
   );
 }
---
+++ Frontend/src/components/login.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+//import { useState } from "react";
+import "./login.css";
+//import user_icon from "../assets/person.png";
+//import email_icon from "../assets/email.png";
+function Login() {
+  return (
+    <div className="login-container">
+      <div className="header">
+        <h1 className="text-head">Welcome Back</h1>
+        <h1 className="text">Email ID</h1>
+        <input type="text" className="mail" />
+        <h1 className="text">Password</h1>
+        <input type="password" className="mail" />
+      </div>
+    </div>
+  );
+}
+
+export default Login;
