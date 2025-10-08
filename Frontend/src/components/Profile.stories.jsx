7a2600895f9119c97b80111e0b00cef6d8980feb
Sanjai-Magilan <sanjai6115@gmail.com>
2025-10-08 13:00:29 +0530

    story book implemented

---
+++ Frontend/src/components/Profile.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Profile from "./Profile";
+
+export default {
+  title: "Components/Profile",
+  component: Profile,
+};
+
+export const Default = () => <Profile />;
