11574509ad003b91b12668d90480464aade35197
Sanjai-Magilan <sanjai6115@gmail.com>
2025-11-03 21:15:41 +0530

    add book

---
+++ Frontend/src/components/AddBook.jsx
@@ -0,0 +1,12 @@
+import react from 'react';
+import "./SearchBar.css";
+
+function AddBook() {
+  return (
+    <div>
+      <h1 className="arcade">Manage Books</h1>
+    </div>
+  );
+}
+
+export default AddBook;
---
+++ Frontend/src/components/AddBook.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AddBook from "./AddBook";
+
+export default {
+  title: "Components/AddBook",
+  component: AddBook
+};
+
+export const Default = () => <AddBook />;
